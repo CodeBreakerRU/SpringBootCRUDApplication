eef13de3e23fc7fac93a73990c099a86b495b154
Rasika Liyanaarachchi <liyanaarachchirasika@gmail.com>
2020-04-18 01:30:14 +0530

    Code optimize

--- abcApplication/src/main/java/com/mtit/abcApplication/Product/repo/ProductRepository.java
+++ abcApplication/src/main/java/com/mtit/abcApplication/Product/repo/ProductRepository.java
@@ -2,6 +2,7 @@ package com.mtit.abcApplication.Product.repo;
 
 import com.mtit.abcApplication.Product.Domain.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 
 public interface ProductRepository extends JpaRepository<Product,Integer> {
